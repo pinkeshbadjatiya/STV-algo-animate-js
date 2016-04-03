a9ca182c918e8acb3cb87b7cf4a383caeda9f790
Pinkesh Badjatiya <pinkeshbadjatiya@gmail.com>
2016-04-03 11:05:08 +0530

    Precise float calculation upto 40 dp

---
+++ README.md
@@ -0,0 +1,4 @@
+## STV algo
+
+1. `result.js` -> Simple implementation of STV algo with poor precision, but works :)
+2. `result_precise.js`  -> Precise calculation of floats upto 40 decimal places.
---
+++ result_precise.js
@@ -0,0 +1,130 @@
+var N = 3;      // No of delegations
+var B = [
+        [1,2,3,4,5],
+        [1,3,2,4,5],
+        [1,2,3,4,5],
+        [4,1,2,3,5],
+        [4,2,3,1,5],
+        [2,4,1,5,3],
+        [2,4,1,3,5],
+        [2,3,1,4,5],
+        [2,1,3,4,5],
+        [2,1,3,4,5]
+];
+var CandidateMap = {
+    1: "Suhas",
+    2: "Mukul Hase",
+    3: "Jerin Philip",
+    4: "Pinkesh",
+    5: "Vinay"
+};
+var S = Big(B.length);
+var t = Big(S).div((N+1)).plus(1).round(0,0);       // equivalent to Math.floor(stuff)..
+var count = {};
+
+
+function printB() {
+    for(var i=0; i<B.length;i++) {
+        console.log(B[i]);
+    }
+}
+
+function initDS() {
+    Big.DP = 40;        // The maximum number of decimal places of the results of operations involving division.
+
+    for(i in CandidateMap) {          // Init count to zero
+        count[i] = Big(0);
+        // createCandidate(i,CandidateMap[i]);
+    }
+    for (var i=0;i<B.length;i++) {      //Initial Counting
+        count[B[i][0]] = (count[B[i][0]]).plus(1.0);
+    }
+}
+
+
+function NextTopCand(dict) {
+    var key, val = Big(-1);
+    for(i in dict) {
+       if ((dict[i]).gte(val)) {
+           val = dict[i];
+           key = i;
+       }
+    }
+    return key;
+}
+
+function NextBottomCand(dict) {
+    var key, val = Big(9999999);
+    for(i in dict) {
+       if ((dict[i]).lt(val)) {
+           val = dict[i];
+           key = i;
+       }
+    }
+    return key;
+}
+
+
+//Function to Transfer votes from Ci, who got k >= t votes, and was qualified to the delegation.
+function TransferDown(c, k) {
+    var f = (Big(k).sub(t)).div(k);      // Fraction to transfer - The higher the precision, the better it is :)
+    for(var i=0; i<B.length;i++) {
+        if(B[i][0] == c) {
+            B[i].shift();
+            if (B[i].length)    // Vote queue becomes empty after last candidate.
+                count[B[i][0]] = count[B[i][0]].plus(f);
+        }
+    }
+}
+
+
+//Function to Transfer votes from 𝕔i, who got least votes in a stage, and was eliminated.
+function TransferUp(c) {
+    for(var i=0; i<B.length;i++) {
+        if(B[i][0] == c) {
+            B[i].shift();
+            if (B[i].length)    // Vote queue becomes empty after last candidate.
+                count[B[i][0]] = count[B[i][0]].plus(1.0);
+        }
+    }
+}
+
+function removeTrace(cand) {
+    delete(count[cand]);                    // Remove from count dict
+    for(var i=0; i<B.length; i++) {          // Remove his trace from voteQueues
+        var ind = B[i].indexOf(parseInt(cand));
+        if (ind == -1)
+            continue;
+        B[i].splice(ind, 1);
+    }
+}
+
+function Qualify(TopCand) {
+    console.log('Winner:',TopCand);
+}
+
+function Looser(LastCand) {
+    console.log('Looser:',LastCand);
+    // frontRemove(LastCand);
+}
+
+
+function calcResult() {         // Delegation Determination
+    var n=0, TopCand, MaxVotes;
+    while(n < N) {
+        TopCand = NextTopCand(count);
+        MaxVotes = count[TopCand];
+        // updateStatus('Next Candidate is: ', CandidateMap[TopCand], 'with',MaxVotes);
+        if( MaxVotes.gte(t)) {
+            Qualify(TopCand);
+            TransferDown(TopCand, MaxVotes);
+            n += 1;
+        } else {
+            // Get the bottom candidate as Top :P
+            TopCand = NextBottomCand(count);
+            Looser(TopCand);
+            TransferUp(TopCand);
+        }
+        removeTrace(TopCand);
+    }
+}
